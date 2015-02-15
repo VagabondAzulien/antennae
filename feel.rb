225c846857174afd50a03cf280e7df4bcf0b8430
Bill Nibz <azulien@gmail.com>
2015-02-14 22:48:51 -0500

    Updating

--- feel.rb
+++ feel.rb
@@ -1,33 +1,50 @@
+#feel -- get the info
+#bill_niblock@yahoo.com
+
 require 'rubygems'
 require 'json'
 require 'net/http'
 
+#Establish a few constants
+Twitch_baseurl = "https://api.twitch.tv/kraken"
+Twitch_clientid = "dejgwfi5bg3m1js5y6yjedp985tc5rm"
+
+########################################################
+# CHANGE THIS TO MODIFY WHO WE'RE GETTING THE INFO FOR #
+########################################################
+Twitch_username = "vagabondazulien"
+
+
 def getStreams(user)
 	#Establish few constants
-	base_url = "https://api.twitch.tv/kraken/"
-	client_id = "some really big number"
-	head = {"Accept"=>"application/vnd.twitch.v2+json"} #, "Client-ID"=>client_id }
+	head = {"Accept"=>"application/vnd.twitch.v2+json", "Client-ID"=>Twitch_clientid }
 
 	#Compose the URL
-	url = "#{base_url}/users/#{URI.encode(user)}/follows/channels"
+	url = "#{Twitch_baseurl}/users/#{URI.encode(user)}/follows/channels"
 
 	#Make the request
-	req = http.get(URI.parse(url), initheader = head)
+	#req = Net::HTTP.get_response(URI.parse(url))
+	http = Net::HTTP.new(url.host, url.port)
+	req = http.get(url, initheader = head)
 	data = req.body
 
-	#convert to Ruby hash
+	#Convert to Ruby hash
 	result = JSON.parse(data)
 
-	#check for errors
+	#Check for errors
 	if result.has_key? 'error'
-		raise "Shit broke"
+		raise "Nope... something didn't work..."
 	end
 	
-	#gimme the money Lebowski!
+	#Gimme the money Lebowski!
 	return result
 end
 
-streamers = getStreams("vagabondazulien")
+def getLive(streams)
+
+end
+
+streamers = getStreams(Twitch_username)
 
 streamers["follows"].each {|key| 
 	puts key["channel"]["name"]}
