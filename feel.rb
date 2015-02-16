08ebe58cf170811c79168be3437275d37ba11a2d
Bill Nibz <azulien@gmail.com>
2015-02-15 23:12:20 -0500

    Updated to use typheous. Still being rate-limited. Working on other things

--- feel.rb
+++ feel.rb
@@ -4,6 +4,7 @@
 require 'rubygems'
 require 'json'
 require 'net/http'
+require 'typhoeus'
 
 #Establish a few constants
 Twitch_baseurl = "https://api.twitch.tv/kraken"
@@ -17,16 +18,23 @@ Twitch_username = "vagabondazulien"
 
 def getStreams(user)
 	#Establish few constants
-	head = {"Accept"=>"application/vnd.twitch.v2+json", "Client-ID"=>Twitch_clientid }
+	#head = {"Accept"=>"application/vnd.twitch.v2+json", "Client-ID"=>Twitch_clientid }
 
 	#Compose the URL
 	url = "#{Twitch_baseurl}/users/#{URI.encode(user)}/follows/channels"
 
 	#Make the request
 	#req = Net::HTTP.get_response(URI.parse(url))
-	http = Net::HTTP.new(url.host, url.port)
-	req = http.get(url, initheader = head)
-	data = req.body
+	#http = Net::HTTP.new(url.host, url.port)
+	#req = http.get(url, initheader = head)
+	#data = req.body
+
+	#Try it with Typhoeus
+	request = Typhoeus::Request.new(
+		"#{url}",
+		headers: {"Accept"=>"application/vnd.twitch.v2+json", "Client-ID"=>"#{Twitch_clientid}" }
+	).run
+	data = request.body
 
 	#Convert to Ruby hash
 	result = JSON.parse(data)
@@ -37,14 +45,20 @@ def getStreams(user)
 	end
 	
 	#Gimme the money Lebowski!
-	return result
+	return result["follows"]
 end
 
 def getLive(streams)
+	live = Hash.new
 
 end
 
 streamers = getStreams(Twitch_username)
 
-streamers["follows"].each {|key| 
-	puts key["channel"]["name"]}
+test_length = 0
+
+streamers.each {|key| 
+	puts key["channel"]["name"]
+	puts streamers.length - test_length
+	test_length += 1}
+
